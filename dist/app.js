2c2947739f44f30dfbfd42aa35c28a1096e78efc
GradyCongdon <gradycongdon@gmail.com>
2019-09-07 22:37:00 -0400

    mongodb

--- dist/app.js
+++ dist/app.js
@@ -8,10 +8,49 @@ const serve = require('koa-static-server');
 app.use(router.routes()).use(router.allowedMethods());
 app.use(serve({ rootDir: 'static' }));
 
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const mongo_password = process.env.MONGO_PASSWORD;
+const url = `mongodb+srv://grady:${mongo_password}@cluster0-f65qf.mongodb.net/test?retryWrites=true&w=majority`;
+const dbName = 'tsmb';
+const client = new MongoClient(url);
+
+let db;
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log('Connected successfully to server');
+  db = client.db(dbName);
+});
+
 app.listen(3000);
 console.log('on port 3k');
 
+router.get('/tsmb-init', async ctx => {
+  const collection = db.collection('posts');
+  const result = await collection.insertMany([getPosts()]);
+  console.log(result);
+  ctx.body = result;
+});
+
 router.get('/api', async ctx => {
+  const collection = db.collection('posts');
+  const postsArray = await collection.find({}).toArray();
+  const posts = postsArray[0];
+  delete posts['_id'];
+  ctx.body = posts;
+});
+
+router.get('/fake', async ctx => {
+  ctx.body = getPosts();
+});
+
+router.get('/hi', async ctx => {
+  ctx.body = {
+    message: 'hello',
+  };
+});
+
+function getPosts() {
   const a = {
     time: Date.now(),
     count: 1,
@@ -32,13 +71,7 @@ router.get('/api', async ctx => {
     text: 'trois',
     favorite: false,
   };
-  ctx.body = {
+  return {
     posts: [a, b, c],
   };
-});
-
-router.get('/hi', async ctx => {
-  ctx.body = {
-    message: 'hello',
-  };
-});
+}
